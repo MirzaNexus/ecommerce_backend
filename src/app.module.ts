5b453b46282b2ab2a47b9a125b849df29d583eec
Mirza Kamran Baig <kamranbaig191@gmail.com>
2026-03-05 00:22:38 +0500

    user module schema created with global response interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { DatabaseModule } from './database/database.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, EventEmitterModule.forRoot()],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    EventEmitterModule.forRoot(),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
